192122f457f67232f97283b642e7745bd26d8fc9
Iskren Ivanov <65283488+iskrenivanov87@users.noreply.github.com>
2020-08-13 11:07:28 +0300

    Add files via upload

---
+++ 02. Stock.py
@@ -0,0 +1,14 @@
+data = input().split(' ')
+bakery = {}
+
+for i in range(0, len(data), 2):
+    key = data[i]
+    value = data[i + 1]
+    bakery[key] = int(value)
+
+stock = input().split(' ')
+for product in stock:
+    if product in bakery:
+        print(f'We have {bakery[product]} of {product} left')
+    else:
+        print(f"Sorry, we don't have {product}")
