ea6149df2abba73c70572ab93879fbd48e6fff4d
Iskren Ivanov <65283488+iskrenivanov87@users.noreply.github.com>
2020-08-31 01:13:22 -0700

    Add files via upload

---
+++ 03. Palindrome Strings.py
@@ -0,0 +1,14 @@
+
+
+def is_palindrome(word):
+    return word == word[::-1]
+
+
+words = input().split()
+search_word = input()
+
+palindrome_words = [word for word in words if is_palindrome(word)]
+count = palindrome_words.count(search_word)
+
+print(palindrome_words)
+print(f'Found palindrome {count} times')
