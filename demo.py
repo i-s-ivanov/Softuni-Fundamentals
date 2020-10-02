ad964f91072d199cffc944ace1bde76790c638f5
Iskren Ivanov <65283488+iskrenivanov87@users.noreply.github.com>
2020-10-02 17:03:40 +0100

    Add files via upload

--- demo.py
+++ demo.py
@@ -1,57 +1,18 @@
-def is_valid_index(length, index):
-    return 0 <= idx < len(war_ship)
+n = int(input())
+guests = []
+arrived_guests = []
 
+for _ in range(n):
+    guest = input()
+    guests.append(guest)
 
-pirate_ship = list(map(int, input().split('>')))
-war_ship = list(map(int, input().split('>')))
-length_warship = len(war_ship)
-length_pirateship = len(pirate_ship)
-max_health = int(input())
+cmd = input()
+while cmd != "END":
+    arrived_guests.append(cmd)
+    cmd = input()
 
-command_input = input()
-
-while command_input != 'Retire':
-    tokens = command_input.split()
-    command = tokens[0]
-
-    if command == 'Fire':
-        idx = int(tokens[1])
-        damage = int(tokens[2])
-
-        if not is_valid_index(length_warship, idx):
-            command_input = input()
-            continue
-        war_ship[idx] -= damage
-        if war_ship[idx] <= 0:
-            print(f'You won! The enemy ship has sunken.')
-            break
-    elif command == 'Defend':
-        first_index = int(tokens[1])
-        second_index = int(tokens[2])
-        damage = int(tokens[3])
-
-        if not (is_valid_index(length_pirateship, first_index) and is_valid_index(length_pirateship, second_index)):
-            command_input = input()
-            continue
-
-        for i in range(first_index, second_index + 1):
-            pirate_ship[i] -= damage
-
-            if pirate_ship[i] <= 0:
-                print(f'You lost! The pirate ship has sunken.')
-                break
-    elif command == 'Repair':
-        index = int(tokens[1])
-        health = int(tokens[2])
-
-        if not is_valid_index(length_pirateship, index):
-            command_input = input()
-            continue
-
-        pirate_ship[index] += health
-
-        if pirate_ship[index]
-    elif command == 'Status':
-        pass
-
-    command_input = input()
+did_not_come = set(guests) - set(arrived_guests)
+did_not_come = sorted(did_not_come)
+print(len(did_not_come))
+[print(x) for x in did_not_come if x[0].isdigit()]
+[print(x) for x in did_not_come if x[0].isalpha()]
