ae439305d6d0fed0b18b4b2052d81d76d3bf20a7
Iskren Ivanov <65283488+iskrenivanov87@users.noreply.github.com>
2020-10-21 12:36:46 +0100

    Add files via upload

---
+++ 2. Value cannot be Negative.py
@@ -0,0 +1,12 @@
+class ValueCannotBeNegative(Exception):
+    def __init__(self, value):
+        self.value = value
+        msg = f'Value {value} is negative'
+        super(ValueCannotBeNegative, self).__init__(msg)
+
+
+n = 5
+for _ in range(n):
+    x = int(input())
+    if x < 0:
+        raise ValueCannotBeNegative(x)
