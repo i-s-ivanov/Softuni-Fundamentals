0ab70f29c433aa71689221b13442d01b4d3eb7e1
Iskren Ivanov <65283488+iskrenivanov87@users.noreply.github.com>
2020-11-16 16:21:56 +0000

    Delete 03. Wolf In Sheep's Clothing.py

--- 03. Wolf In Sheep's Clothing.py
+++
@@ -1,8 +0,0 @@
-text = input().split(', ')
-
-for i in range(len(text)):
-    if text[i] == "wolf":
-        if i == len(text) - 1:
-            print(f'Please go away and stop eating my sheep')
-        else:
-            print(f'Oi! Sheep number {len(text) - (i + 1)}! You are about to be eaten by a wolf!')
