2c93fd089e782372c4f605a0aae00ea43b3e4cee
Iskren Ivanov <65283488+iskrenivanov87@users.noreply.github.com>
2020-12-15 21:54:56 +0000

    Delete 03. Capitals.py

--- 03. Capitals.py
+++
@@ -1,4 +0,0 @@
-countries = input().split(', ')
-capitals = input().split(', ')
-result = {pair[0]: pair[1] for pair in zip(countries, capitals)}
-[print(f'{key} -> {value}') for key, value in result.items()]
