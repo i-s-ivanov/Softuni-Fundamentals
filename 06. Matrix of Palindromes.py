d94a8ea42ab817f6e1e7dc51531b4d38756c9595
Iskren Ivanov <65283488+iskrenivanov87@users.noreply.github.com>
2021-01-28 18:37:13 +0000

    Delete 06. Matrix of Palindromes.py

--- 06. Matrix of Palindromes.py
+++
@@ -1,4 +0,0 @@
-rows, cols = [int(x) for x in input().split()]
-
-matrix = [[f"{chr(row)}{chr(row + col)}{chr(row)}" for col in range(cols)] for row in range(97, 97 + rows)]
-[print(' '.join(row)) for row in matrix]
